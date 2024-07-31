8bc1bc83249a8d2535c42a2ee8bffece0a797f01
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-07-31 09:17:31 -0600

    En Receta agregue un public class Receta que nos deja con Nombre, Ingredientes e Instrucciones

--- Receta.cs
+++ Receta.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace MenuReceta
 {
-    internal class Receta
+    public class Receta
     {
+        public string Nombre { get; set; }
+        public string Ingredientes { get; set; }
+        public string Instrucciones { get; set; }
+
+        public Receta(string nombre, string ingredientes, string instrucciones)
+        {
+            Nombre = nombre;
+            Ingredientes = ingredientes;
+            Instrucciones = instrucciones;
+        }
+
     }
 }
