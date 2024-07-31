40158fbe7ffba80115b8f74e2ee1a2b5fe817c96
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-07-31 09:21:07 -0600

    se hizo un menú con varias opciones para agregar receta, buscar receta, mostrar todas las recetas y salir del programa

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
-﻿class MenuRecetas
+﻿using MenuReceta;
+
+class MenuRecetas
 {
-    static void Main(string[] args )
+    static void Main(string[] args)
     {
-        
+        ColeccionDeRecetas coleccion = new ColeccionDeRecetas();
+
+        while (true)
+        {
+            Console.WriteLine("Menú:");
+            Console.WriteLine("1. Agregar Receta");
+            Console.WriteLine("2. Buscar Receta por Nombre");
+            Console.WriteLine("3. Mostrar Todas las Recetas");
+            Console.WriteLine("4. Salir");
+            Console.Write("Seleccione una opción: ");
+
+            int opcion;
+            if (!int.TryParse(Console.ReadLine(), out opcion))
+            {
+                Console.WriteLine("Opción no válida. Intente nuevamente.");
+                continue;
+            }
+
+        }
     }
 }
